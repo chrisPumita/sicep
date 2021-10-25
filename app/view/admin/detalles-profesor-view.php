9a809d56e0c5339167f9f749783f193414bd5d6e
Christan RCSG <christian.floppy@gmail.com>
2021-10-25 01:01:24 -0500

    details profesor

--- app/view/admin/detalles-profesor-view.php
+++ app/view/admin/detalles-profesor-view.php
@@ -130,6 +130,57 @@
                         </div>
                     </div>
                 </div>
+                <!-- ADMIN ACCOUNT ENABLE-->
+                <div class="section">
+                    <div class="card">
+                        <div class="card-header img_bg_cards" style="background-image: url(../assets/images/icons/group3.svg);">
+                            <div class="col-12" >
+                                <h4 class="card-title "><b>CUENTA DE ADMINISTRADOR</b></h4>
+                            </div>
+                            <div class="col" >
+                                <h6 class="card-subtitle mb-2 text-muted">
+                                    <p class="card-text text-muted small ">
+                                    <div class="spinner-grow bg-success" role="status" style="width: 1rem; height: 1rem"></div>
+                                    Este profesor tiene cuenta de adminsitrador <span class="vl ml-1 mr-2 "></span>
+                                    desde el <span class="font-weight-bold"> 15 Octubre de 2021</span></p>
+                                </h6>
+                            </div>
+                        </div>
+                        <div class="card-body" >
+                            <div class="row">
+                                <div class="col">
+                                    <div class="list-group">
+                                        <button type="button" class="list-group-item list-group-item-action">Cargo: Coordinador</button>
+                                        <button type="button" class="list-group-item list-group-item-action">Nivel de permiso: Bajos</button>
+                                        <button type="button" class="list-group-item list-group-item-action">Clave:
+                                            4a7d1ed414474e4033ac29ccb8653d9b</button>
+                                        <button type="button" class="list-group-item list-group-item-action">Porta
+                                            ac
+                                            consectetur
+                                            ac</button>
+                                        <button type="button" class="list-group-item list-group-item-action">Vestibulum at
+                                            eros</button>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="card-footer">
+                            <div class="row">
+                                <div class=" col-md-auto ">
+                                    <a href="#" class="btn btn-primary btn-black">
+                                        <i class="fas fa-edit"></i>
+                                        <small>EDITAR</small></a>
+                                </div>
+                                <div class=" col-md-auto ">
+                                    <a href="#" class="btn btn-danger btn-black">
+                                        <i class="fas fa-ban"></i>
+                                        <small>Deshabilitar</small></a>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- ADMIN ACCOUNT ENABLE-->
             </section>
 
             <section class="section">
