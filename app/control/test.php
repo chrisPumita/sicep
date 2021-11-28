8e118da922fc3e7433479189e42815b6d3b2ff0b
htmtmx <cesar.hpp96@hotmail.com>
2021-11-27 19:36:34 -0600

    test.php

---
+++ app/control/test.php
@@ -0,0 +1,5 @@
+<?php
+include_once "./controlAsignaciones.php";
+//var_dump(consultaAsignacionGrupos(1));
+//var_dump(consultaNumSolicitudesInscripcion(2));
+var_dump(ConsultaNumLugaresDisponibles(2));
