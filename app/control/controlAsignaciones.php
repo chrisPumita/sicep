a0836425c2345e7955a23bef51789b9ff6e9aea5
htmtmx <cesar.hpp96@hotmail.com>
2021-11-27 19:33:05 -0600

    Informacion de cada asignacion, num lugares disponibles y num de solicitudes de inscripcion

--- app/control/controlAsignaciones.php
+++ app/control/controlAsignaciones.php
@@ -1,1 +1,24 @@
 <?php
+function consultaAsignacionGrupos($valorFiltro)
+{
+    include_once "../model/ASIGNACION_GRUPO.php";
+    $ASIGNACION_GRUPO = new ASIGNACION_GRUPO();
+    $result = $ASIGNACION_GRUPO->queryConsultaAsignacionGrupos($valorFiltro);
+    return $result;
+}
+
+function consultaNumSolicitudesInscripcion($id_asignacion)
+{
+    include_once "../model/ASIGNACION_GRUPO.php";
+    $ASIGNACION_GRUPO = new ASIGNACION_GRUPO();
+    $result = $ASIGNACION_GRUPO->queryconsultaNumSolicitudesInscripcion($id_asignacion);
+    return $result;
+}
+
+function ConsultaNumLugaresDisponibles($id_asignacion)
+{
+    include_once "../model/ASIGNACION_GRUPO.php";
+    $ASIGNACION_GRUPO = new ASIGNACION_GRUPO();
+    $result = $ASIGNACION_GRUPO->queryConsultaNumLugaresDisponibles($id_asignacion);
+    return $result;
+}
