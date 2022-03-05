67ad1891d8cb94b7aaf2c3db03ec12b3e97366e6
Jeniper70 <jeniper.666@outlook.com>
2022-03-05 14:11:29 -0600

    Cambios minimos
    Co-Authored-By: Christan RCSG <christian.floppy@gmail.com>

--- app/ficha_inscripcion/index.php
+++ app/ficha_inscripcion/index.php
@@ -6,32 +6,47 @@ if (!isset($_SESSION['usuario'])) {
     header('Location: ../');
 } else {
     if (isset($_POST['id'])) {
-        $noFicha = $_POST['id'];
-        require_once '../../vendor/autoload.php';
-        include_once '../control/control_fichas_pdf.php';
-        $mpdfConfig = array(
-            'mode' => 'utf-8',
-            'format' => 'letter',    // format - A4, for example, default ''
-            'default_font_size' => 0,     // font size - default 0
-            'default_font' => '',    // default font family
-            'margin_left' => 0,        // 15 margin_left
-            'margin_right' => 0,        // 15 margin right
-            'margin_top' => 0,
-            // 'mgt' => $headerTopMargin,     // 16 margin top
-            // 'mgb' => $footerTopMargin,    	// margin bottom
-            'margin_header' => 0,     // 9 margin header
-            'margin_footer' => 0,     // 9 margin footer
-            'orientation' => 'P'    // L - landscape, P - portrait
-        );
-        $mpdf = new \Mpdf\Mpdf($mpdfConfig);
-        $plantilla = getBodyFicha($noFicha);
-        $css = file_get_contents('../../assets/css/ficha_style.css');
-        $mpdf->writeHtml($css, \Mpdf\HTMLParserMode::HEADER_CSS);
-        $mpdf->writeHtml($plantilla, \Mpdf\HTMLParserMode::HTML_BODY);
-        $mpdf->SetTitle("Ficha de Inscripcion");
-        $mpdf->SetAuthor('CHRIS');
-        $mpdf->SetProtection(array('copy', 'print'), '', 'MyPassword');
-        $mpdf->Output('ficha_inscripcion.pdf', 'I');
+        $idAsig = $_POST['id'];
+        $idAlumno = $_SESSION['id_alumno'];
+        include_once "../control/controlInscripciones.php";
+        $ficha = confirmaMatchSolicitudAlumno($idAsig,$idAlumno);
+        if (count($ficha)>0)
+        {
+            var_dump($ficha);
+            /*
+            
+            
+            */
+            require_once '../../vendor/autoload.php';
+            include_once '../control/control_fichas_pdf.php';
+            $mpdfConfig = array(
+                'mode' => 'utf-8',
+                'format' => 'letter',    // format - A4, for example, default ''
+                'default_font_size' => 0,     // font size - default 0
+                'default_font' => '',    // default font family
+                'margin_left' => 0,        // 15 margin_left
+                'margin_right' => 0,        // 15 margin right
+                'margin_top' => 0,
+                // 'mgt' => $headerTopMargin,     // 16 margin top
+                // 'mgb' => $footerTopMargin,    	// margin bottom
+                'margin_header' => 0,     // 9 margin header
+                'margin_footer' => 0,     // 9 margin footer
+                'orientation' => 'P'    // L - landscape, P - portrait
+            );
+            $mpdf = new \Mpdf\Mpdf($mpdfConfig);
+            $plantilla = getBodyFicha($idAsig);
+            $css = file_get_contents('../../assets/css/ficha_style.css');
+            $mpdf->writeHtml($css, \Mpdf\HTMLParserMode::HEADER_CSS);
+            $mpdf->writeHtml($plantilla, \Mpdf\HTMLParserMode::HTML_BODY);
+            $mpdf->SetTitle("Ficha de Inscripcion");
+            $mpdf->SetAuthor('CHRIS');
+            $mpdf->SetProtection(array('copy', 'print'), '', 'MyPassword');
+        //    $mpdf->Output('ficha_inscripcion.pdf', 'I');
+        }
+        else
+        {
+           echo "El archivo no es tuyo";
+        }
     } else {
         header('Location: ../');
     }
--- app/model/INSCRIPCION.php
+++ app/model/INSCRIPCION.php
@@ -364,7 +364,7 @@ FROM `persona` per INNER JOIN `profesor` prof WHERE  prof.`id_persona_fk`=per.`i
           AND asig.id_asignacion = insc.id_asignacion_fk
           AND gpo.id_grupo = asig.id_grupo_fk
           AND gpo.id_curso_fk = c.id_curso ".$FiltroAlumno." ".$filtro." ".$FiltroInsc." ORDER BY insc.fecha_solicitud DESC";
-
+            echo $sql;
         $this->connect();
         $result = $this->getData($sql);
         $this->close();
